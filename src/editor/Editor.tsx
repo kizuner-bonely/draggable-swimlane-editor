679bfbe1ae81fba7f900dfa83a808f6942f05d5d
harold <1003306162@qq.com>
2021-04-08 15:58:22 +0800

    A 添加在同一列表变换顺序的功能;

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,7 +1,7 @@
 import type { MenuProps } from './EditorMenu'
 import EditorMenu from './EditorMenu'
 import EditorContent from './EditorContent'
-import { useCallback, useState } from 'react'
+import { useCallback } from 'react'
 import { DragDropContext, DropResult } from 'react-beautiful-dnd'
 import { connect } from 'react-redux'
 import { RootDispatch, RootState } from '@/store/store'
@@ -16,7 +16,7 @@ const reorder = (list: any[], startIndex: number, endIndex: number) => {
   return res
 }
 
-function Editor({ swimLanes, menu, addList, removeList }: EditorProps) {
+function Editor({ swimLanes, menu, addList, removeList, updateList }: EditorProps) {
   /*
   * result 结构
   *   combine: null;
@@ -36,17 +36,20 @@ function Editor({ swimLanes, menu, addList, removeList }: EditorProps) {
   const onDragEnd = useCallback((result: DropResult) => {
     console.log(result)
     const { source, destination } = result
-    //* 将可拖拽的组件拉到可拖拽区域以外的地方
-    if (!destination) return
+    //* 将可拖拽的组件拉到可拖拽区域以外的地方或者菜单栏时动作无效
+    if (!destination || destination.droppableId === 'menu') return
     //* 在当前区域调整顺序
     if (source.droppableId === destination.droppableId) {
-    
+      const _swimLanes = JSON.parse(JSON.stringify(swimLanes))
+      const list = _swimLanes.find((v: {title: string}) => v.title === destination.droppableId)
+      const newList = reorder(list.contents, source.index, destination.index)
+      updateList({title: destination.droppableId, contents: newList})
     } else {
       //* 将当前区域的元素拉到其他区域
       
     }
     
-  }, [])
+  }, [swimLanes, updateList])
   
   return (
     <div className={styles.editor}>
@@ -66,6 +69,7 @@ const mapStateToProps = (state: RootState) => ({
 const mapDispatchToProps = (dispatch: RootDispatch) => ({
   addList: dispatch.swimLanes.add,
   removeList: dispatch.swimLanes.remove,
+  updateList: dispatch.swimLanes.update,
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Editor)
