d737f5d7ab6d8d6a959a17d43ed5b9c10f04cd30
harold <1003306162@qq.com>
2021-04-12 19:29:41 +0800

    A 添加连线删除功能;

--- src/editor/mxgraph/MxgraphDemo.jsx
+++ src/editor/mxgraph/MxgraphDemo.jsx
@@ -2,6 +2,7 @@
 import { PureComponent } from 'react'
 import './mxgraphDemo.less'
 import 'jsplumb'
+import common from './config'
 
 export default class MxgraphDemo extends PureComponent {
   state = {
@@ -9,47 +10,17 @@ export default class MxgraphDemo extends PureComponent {
   }
 
   componentDidMount() {
-    const common = {
-      isSource: true,
-      isTarget: true,
-      connector: ['Flowchart'],
-      maxConnections: -1,
-      endpoint: 'Dot',
-      paintStyle: {
-        fill: 'transparent',
-        outlineStroke: 'blue',
-        strokeWidth: 1,
-      },
-      hoverPaintStyle: {
-        outlineStroke: 'lightblue',
-        strokeWidth: 1,
-      },
-      connectorStyle: {
-        outlineStroke: 'green',
-        strokeWidth: 1,
-      },
-      connectorHoverStyle: {
-        strokeWidth: 2,
-      },
-    }
-
     jsPlumb.ready(function () {
       jsPlumb.addEndpoint('item_left', { anchors: ['Right'] }, common)
       jsPlumb.addEndpoint('item_left', { anchors: ['Top'] }, common)
       jsPlumb.addEndpoint('item_right', { anchors: ['Left'] }, common)
-      // jsPlumb.connect({
-      //   source: 'item_left',
-      //   target: 'item_right',
-      //   endpoint: 'Blank',
-      //   connector: ['Flowchart'],
-      //   overlays: [['Arrow', { width: 12, length: 12, location: 1 }]],
-      // })
 
       jsPlumb.draggable('item_left', { containment: 'diagramContainer' })
       jsPlumb.draggable('item_right', { containment: 'diagramContainer' })
 
       jsPlumb.bind('click', function (conn, originalEvent) {
-        if (window.prompt('输入1删除连接') === '1') jsPlumb.detach(conn)
+        if (window.prompt('输入1删除连接') === '1')
+          jsPlumb.deleteConnection(conn)
       })
     })
   }
